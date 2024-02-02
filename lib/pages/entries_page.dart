6a88532e89cbb03c19543d7e2526ff2a81b48945
mchlsbl <92347868+mchlsbl@users.noreply.github.com>
2024-02-02 19:17:23 +0100

    Add sorting and grouping by measurement time

--- lib/pages/entries_page.dart
+++ lib/pages/entries_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 import 'package:systolic/models/entry_database.dart';
 import 'package:systolic/models/entry.dart';
@@ -127,11 +128,37 @@ class _EntriesPageState extends State<EntriesPage> {
     context.read<EntryDatabase>().deleteEntry(id);
   }
 
+  List<Entry> sortEntriesByTime(List<Entry> entries) {
+    entries.sort((a, b) => b.time.compareTo(a.time));
+    return entries;
+  }
+
+  Map<String, List<Entry>> groupEntriesByDay(List<Entry> entries) {
+    Map<String, List<Entry>> groupedEntries = {};
+
+    entries.sort((a, b) => b.time.compareTo(a.time));
+
+    for (var entry in entries) {
+      String dayKey = DateFormat('dd.MM.yyyy')
+          .format(DateTime.fromMillisecondsSinceEpoch(entry.time));
+
+      if (groupedEntries.containsKey(dayKey)) {
+        groupedEntries[dayKey]!.add(entry);
+      } else {
+        groupedEntries[dayKey] = [entry];
+      }
+    }
+
+    return groupedEntries;
+  }
+
   @override
   Widget build(BuildContext context) {
     final entryDatabase = context.watch<EntryDatabase>();
 
     List<Entry> currentEntries = entryDatabase.currentEntries;
+
+    Map<String, List<Entry>> groupedEntries = groupEntriesByDay(currentEntries);
 
     return Scaffold(
       floatingActionButton: FloatingActionButton(
@@ -166,13 +193,39 @@ class _EntriesPageState extends State<EntriesPage> {
                 ),
           Expanded(
             child: ListView.builder(
-              itemCount: currentEntries.length,
+              itemCount: groupedEntries.length,
               itemBuilder: (context, index) {
-                final entry = currentEntries[index];
-                return EntryTile(
-                  entry: entry,
-                  onEditTap: () => updateEntry(entry),
-                  onDeleteTap: () => deleteEntry(entry.id),
+                final List<String> keys = groupedEntries.keys.toList();
+                final String dayKey = keys[index];
+                final List<Entry> entries =
+                    sortEntriesByTime(groupedEntries[dayKey]!);
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20, left: 18),
+                      child: Text(
+                        dayKey,
+                        style: const TextStyle(
+                          fontSize: 22,
+                        ),
+                      ),
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: entries.length,
+                      itemBuilder: (context, index) {
+                        final entry = entries[index];
+                        return EntryTile(
+                          entry: entry,
+                          onEditTap: () => updateEntry(entry),
+                          onDeleteTap: () => deleteEntry(entry.id),
+                        );
+                      },
+                    ),
+                  ],
                 );
               },
             ),
