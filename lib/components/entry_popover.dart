d9db0a1ebeb502530839dc29cc13291b60154851
mchlsbl <92347868+mchlsbl@users.noreply.github.com>
2024-02-07 11:24:37 +0100

    Add Traditional Chinese

--- lib/components/entry_popover.dart
+++ lib/components/entry_popover.dart
@@ -4,11 +4,13 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class EntryPopover extends StatelessWidget {
   final void Function()? onEditTap;
   final void Function()? onDeleteTap;
+  final double? buttonHeight;
 
   const EntryPopover({
     super.key,
     required this.onEditTap,
     required this.onDeleteTap,
+    required this.buttonHeight,
   });
 
   @override
@@ -21,7 +23,7 @@ class EntryPopover extends StatelessWidget {
             onEditTap!();
           },
           child: SizedBox(
-            height: 45,
+            height: buttonHeight,
             child: Center(child: Text(AppLocalizations.of(context)!.edit)),
           ),
         ),
@@ -31,7 +33,7 @@ class EntryPopover extends StatelessWidget {
             onDeleteTap!();
           },
           child: SizedBox(
-            height: 45,
+            height: buttonHeight,
             child: Center(child: Text(AppLocalizations.of(context)!.delete)),
           ),
         ),
