ae6ea4658afac247bec19cf8e60c8fe084dc1bc4
mchlsbl <mchlsbl@proton.me>
2024-02-23 10:32:09 +0100

    Fix prefs bug

--- lib/components/options_tile.dart
+++ lib/components/options_tile.dart
@@ -5,44 +5,62 @@ class OptionsTile extends StatefulWidget {
   final String id;
   final String title;
 
-  const OptionsTile({super.key, required this.id, required this.title});
+  const OptionsTile({
+    super.key,
+    required this.id,
+    required this.title,
+  });
 
   @override
   State<OptionsTile> createState() => _OptionsTileState();
 }
 
 class _OptionsTileState extends State<OptionsTile> {
-  late SharedPreferences prefs;
+  late Future<SharedPreferences> prefsFuture;
 
   @override
   void initState() {
     super.initState();
-    _loadPreferences();
+    prefsFuture = _loadPreferences();
   }
 
-  Future<void> _loadPreferences() async {
-    prefs = await SharedPreferences.getInstance();
+  Future<SharedPreferences> _loadPreferences() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20, left: 15, right: 15),
-      child: Material(
-        color: Theme.of(context).colorScheme.surfaceVariant,
-        borderRadius: BorderRadius.circular(16),
-        clipBehavior: Clip.antiAlias,
-        child: SwitchListTile(
-          contentPadding: const EdgeInsets.only(left: 15, right: 10),
-          title: Text(widget.title),
-          value: prefs.getBool(widget.id) ?? false,
-          onChanged: (bool value) {
-            setState(() {
-              prefs.setBool(widget.id, value);
-            });
-          },
-        ),
-      ),
+    return FutureBuilder(
+      future: prefsFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+        if (snapshot.connectionState != ConnectionState.waiting) {
+          final SharedPreferences prefs = snapshot.data!;
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 20, left: 15, right: 15),
+            child: Material(
+              color: Theme.of(context).colorScheme.surfaceVariant,
+              borderRadius: BorderRadius.circular(16),
+              clipBehavior: Clip.antiAlias,
+              child: SwitchListTile(
+                contentPadding: const EdgeInsets.only(left: 15, right: 10),
+                title: Text(widget.title),
+                value: prefs.getBool(widget.id) ?? false,
+                onChanged: (bool value) {
+                  setState(() {
+                    prefs.setBool(widget.id, value);
+                  });
+                },
+              ),
+            ),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
